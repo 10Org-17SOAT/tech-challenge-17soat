7995232ff8f35dec54f319735c086d13c22555dd
Antônio César <antonioazevedo.dev@gmail.com>
2026-08-16 20:12:12 -0300

    feat: scaffold customer module domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './shared/config/database/database.module';
 import { validateEnv } from './shared/config/env/env.validation';
+import { CustomerModule } from './modules/onboarding/customer/customer.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { validateEnv } from './shared/config/env/env.validation';
       validate: validateEnv,
     }),
     DatabaseModule,
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
