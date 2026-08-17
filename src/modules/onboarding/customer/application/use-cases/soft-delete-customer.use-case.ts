3f112db5df2527c756dfa537ec0b56c001c3a110
Antônio César <antonioazevedo.dev@gmail.com>
2026-08-17 00:19:20 -0300

    feat: add soft delete for customer use case

---
+++ src/modules/onboarding/customer/application/use-cases/soft-delete-customer.use-case.ts
@@ -0,0 +1,18 @@
+import { CustomerRepository } from '../../domain/repository/customer.repository';
+import { CustomerNotFoundException } from '../exceptions/customer-not-found.exception';
+
+export class SoftDeleteCustomerUseCase {
+  constructor(private readonly repository: CustomerRepository) {}
+
+  async execute(input: { id: string }): Promise<void> {
+    const customer = await this.repository.findById(input.id);
+
+    if (!customer) {
+      throw new CustomerNotFoundException(input.id);
+    }
+
+    customer.softDelete();
+
+    await this.repository.save(customer);
+  }
+}
